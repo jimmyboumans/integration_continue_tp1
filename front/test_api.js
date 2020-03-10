dc22caf55db69e559563d7c48d5f642211984cb2
Mtd42 <dorville.mathieu@gmail.com>
2020-03-10 12:30:00 +0100

    update fetch

--- front/test_api.js
+++ front/test_api.js
@@ -5,20 +5,13 @@ const url = {
     groupeContrat: "url here",
 }
 
-function getData(url) {
-    const results = []
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        // TODO : Gérer les codes retour
-        if (this.readyState == 4 && this.status == 200) {
-            const obj_resp = JSON.parse(this.responseText);
-            results.push(obj_resp)
-        }
-    };
-    xmlhttp.open('GET', url, true);
-    xmlhttp.send();
-    return results;
+async function getData(url) {
+    try {
+        const a = await fetch(url)
+        // const resp = await a.json()
+        // console.log(resp)
+        return a.json()
+    } catch(e) {
+        console.error(e)
+    }
 }
-
-const resp = getData(url.articles)
-
